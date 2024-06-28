f4ff47f95d473d685962bb7da18200df88861f0e
Lorena Anyer <claudia.lorena.anyer@gmail.com>
2024-06-28 12:11:50 -0300

    javascrip

---
+++ index.js
@@ -0,0 +1,51 @@
+//comemtarios si le saco las // lo descomento
+/* comentarios/
+/*  las variables son espacios de memoria para almacenar datos/* 
+/*  hasta que no las guardemos no generan cambios 
+/*  ejemplo var es un lenguare que siginifica variable y es solo utilizado e como lenjuague de
+
+/* fin */ //al final de cada palabra que sigue  a la var debe ir ;
+
+var nombre;
+var mail;
+var edad;
+var pais;
+
+// NO podemos comenzar el nombde de una vr con un numero 
+var 123;
+//NO  podemos iniciar con espacios en blanco en el medio
+var nombrea apellido;
+//NO  podemos iniciar con guion medio
+var -LockManager;
+
+
+
+// SI  podemos crear variables con simbolos $ _ guion bajo
+var nombreAPELLIDO //CAMELCASE es el que mas se uda sig primer palabra en minuscula segunda pegada en mayuscula
+var nombre_apellido  // no guion medio 
+
+
+
+// son variables por eso cada provincia puede ser disbntinta y asignar datos de tipos numericos
+provincia = 'caba1';
+provincia ='cordoba';
+provincia = 123; // valor numerico de variable
+provincia = true;//valor buleano es verdadero o falso
+provincia = false;
+
+const
+
+// existen errores de sintaxis ejemplo escribo mal var y pongo va sin la r y error en tiempo de ejecucion es cuando parece que no tengp error de pero cuando ejecuto el programa salta el error
+
+
+console.log(nombre+apellido+provincia);
+//variables
+nombre ='juan';
+apellido ='perez';
+
+const DNI = 1234545;//asigno una constante Y LA tengo que definir en este caso dni(mayuscula) es igual a 123456 el punto y coma puede estar o no el signo igual repre4senta  
+DNI =PEPE;// ya asigne a la contante numeros no le puedo cambiar el valor// el error lo aparece en tiempo de e4jecuiob
+// este error se ve cuanso lo pego en la consola
+// var es un codigo antiguo si nos llega algo para resolver con var trabajaremos con bar si nos llega algo nuebo con 
+const y let
+//= este no es un signo igual el signo == seria el signo igual
---
+++ matematic.js
@@ -0,0 +1,27 @@
+
+
+// Operadores matemáticos
+// + - * / % 
+let numUno = 15;
+let numDos = 50;
+
+let resultado;
+/* let resultadoSuma;
+let resultadoResta;
+let resultadoDivision;
+let resultadoMultiplicacion; */
+
+resultado = numUno + numDos; // ACA PUEDE PONER CUALEUIER VARIABLEOSEA NUMERO
+
+console.log(20 + 50);// si hago la suma 15+50 pero no hago console.log no me va hacer la suma
+
+console.log(numUno + numDos);// aca el signo mas no ocncatena sino que suma
+
+console.log(resultado);
+
+
+resultado = numUno - numDos;
+
+console.log("El resultado de la resta es: " + resultado);// el signo mas concatena une aca no usma pues es numeros y letras
+
+alert("El resultado de la resta es: " + resultado);
